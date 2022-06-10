f7bed33786f8b9f838076d16628e9e8d6f504bda
Marwane Beriich <marber.mb11@gmail.com>
2022-06-10 18:56:54 +0100

    add CORS

--- src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
+++ src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
@@ -1,12 +1,18 @@
 package com.example.inventoryservice;
 
 import com.example.inventoryservice.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.example.inventoryservice.entities.Product;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
 
 
 @SpringBootApplication
@@ -22,12 +28,12 @@ public class InventoryServiceApplication {
         return args ->{
             productRepository.save(new Product(null, "MacBook Pro M2", 12000, 12));
             productRepository.save(new Product(null, "iPhone Pro Max", 10000, 23));
+            productRepository.save(new Product(null, "iMac Pro 2021", 22000, 19));
+            productRepository.save(new Product(null, "iWatch Pro 6", 6700, 43));
             productRepository.save(new Product(null, "iPad Pro 13", 8000, 9));
             productRepository.findAll().forEach(product ->{
                 System.out.println(product.getName());
             });
         };
     }
-
 }
-
--- src/main/java/com/example/inventoryservice/repository/ProductRepository.java
+++ src/main/java/com/example/inventoryservice/repository/ProductRepository.java
@@ -3,8 +3,9 @@ package com.example.inventoryservice.repository;
 import com.example.inventoryservice.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource
+@CrossOrigin(origins="http://localhost:3000")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
